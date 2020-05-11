954ea3df22f23df31404468522e3d0f3fc5aef76
lucassansiq <50505546+lucassansiq@users.noreply.github.com>
2020-05-11 18:49:56 -0300

    Create Principal.java

---
+++ AmbientalQuiz/src/controller/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import model.Jogador;
+
+/**
+ *
+ * @author Lucas Hype
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Lucas");
+        System.out.println(jogador);
+        
+        
+    }
+
+}
