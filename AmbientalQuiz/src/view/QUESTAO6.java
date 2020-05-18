dd5bc8fc9d60342f05bbc49406576b158a275a85
lucassansiq <50505546+lucassansiq@users.noreply.github.com>
2020-05-18 18:57:20 -0300

    Update QUESTAO6.java

--- AmbientalQuiz/src/view/QUESTAO6.java
+++ AmbientalQuiz/src/view/QUESTAO6.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import static view.Cadastro.pontuacao;
+import static view.Cadastro.vidas;
+
 /**
  *
  * @author Lucas
@@ -30,48 +33,68 @@ public class QUESTAO6 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        bt1 = new javax.swing.JButton();
+        bt2 = new javax.swing.JButton();
+        bt3 = new javax.swing.JButton();
+        bt4 = new javax.swing.JButton();
+        btVoltar = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        lbQuestao = new javax.swing.JLabel();
+        lb1 = new javax.swing.JLabel();
+        lb2 = new javax.swing.JLabel();
+        lb3 = new javax.swing.JLabel();
+        lb4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(0, 255, 153));
         jPanel1.setForeground(new java.awt.Color(0, 255, 153));
 
-        jButton1.setBackground(new java.awt.Color(0, 255, 153));
-        jButton1.setForeground(new java.awt.Color(0, 255, 153));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/1.png"))); // NOI18N
+        bt1.setBackground(new java.awt.Color(0, 255, 153));
+        bt1.setForeground(new java.awt.Color(0, 255, 153));
+        bt1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/1.png"))); // NOI18N
+        bt1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt1ActionPerformed(evt);
+            }
+        });
 
-        jButton2.setBackground(new java.awt.Color(0, 255, 153));
-        jButton2.setForeground(new java.awt.Color(0, 255, 153));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/2.png"))); // NOI18N
+        bt2.setBackground(new java.awt.Color(0, 255, 153));
+        bt2.setForeground(new java.awt.Color(0, 255, 153));
+        bt2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/2.png"))); // NOI18N
+        bt2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt2ActionPerformed(evt);
+            }
+        });
 
-        jButton3.setBackground(new java.awt.Color(0, 255, 153));
-        jButton3.setForeground(new java.awt.Color(0, 255, 153));
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/3.png"))); // NOI18N
+        bt3.setBackground(new java.awt.Color(0, 255, 153));
+        bt3.setForeground(new java.awt.Color(0, 255, 153));
+        bt3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/3.png"))); // NOI18N
+        bt3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt3ActionPerformed(evt);
+            }
+        });
 
-        jButton4.setBackground(new java.awt.Color(0, 255, 153));
-        jButton4.setForeground(new java.awt.Color(0, 255, 153));
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/4.png"))); // NOI18N
+        bt4.setBackground(new java.awt.Color(0, 255, 153));
+        bt4.setForeground(new java.awt.Color(0, 255, 153));
+        bt4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/4.png"))); // NOI18N
+        bt4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt4ActionPerformed(evt);
+            }
+        });
 
-        jButton5.setBackground(new java.awt.Color(51, 255, 153));
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/voltar.png"))); // NOI18N
-        jButton5.setToolTipText("");
+        btVoltar.setBackground(new java.awt.Color(51, 255, 153));
+        btVoltar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/voltar.png"))); // NOI18N
+        btVoltar.setToolTipText("");
 
         jPanel2.setBackground(new java.awt.Color(204, 255, 204));
         jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Como consumir de forma consciente?  ");
+        lbQuestao.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbQuestao.setText("6.Como consumir de forma consciente?  ");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -79,28 +102,28 @@ public class QUESTAO6 extends javax.swing.JFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(lbQuestao)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(75, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(lbQuestao)
                 .addContainerGap())
         );
 
-        jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        jLabel2.setText("  Trocando todos os nossos objetos sempre que um novo do mesmo tipo for lançado.");
+        lb1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        lb1.setText("  Trocando todos os nossos objetos sempre que um novo do mesmo tipo for lançado.");
 
-        jLabel3.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        jLabel3.setText("Usar a mangueira para lavar o quintal e o carro.");
+        lb2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        lb2.setText("Usar a mangueira para lavar o quintal e o carro.");
 
-        jLabel4.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        jLabel4.setText("Utilizando os recursos naturais para satisfazer nossas necessidades e das gerações futuras.");
+        lb3.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        lb3.setText("Utilizando os recursos naturais para satisfazer nossas necessidades e das gerações futuras.");
 
-        jLabel5.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        jLabel5.setText("Adquirindo qualquer tipo de produto se for barato.  ");
+        lb4.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        lb4.setText("Adquirindo qualquer tipo de produto se for barato.  ");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -110,21 +133,21 @@ public class QUESTAO6 extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btVoltar, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(20, 20, 20))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton4)
-                    .addComponent(jButton3)
-                    .addComponent(jButton2)
-                    .addComponent(jButton1))
+                    .addComponent(bt4)
+                    .addComponent(bt3)
+                    .addComponent(bt2)
+                    .addComponent(bt1))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5))
+                    .addComponent(lb2)
+                    .addComponent(lb1)
+                    .addComponent(lb3)
+                    .addComponent(lb4))
                 .addContainerGap(149, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -133,23 +156,23 @@ public class QUESTAO6 extends javax.swing.JFrame {
                 .addGap(14, 14, 14)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton5))
+                    .addComponent(btVoltar))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
-                    .addComponent(jLabel2))
+                    .addComponent(bt1)
+                    .addComponent(lb1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3)
-                    .addComponent(jButton2))
+                    .addComponent(lb2)
+                    .addComponent(bt2))
                 .addGap(30, 30, 30)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton3)
-                    .addComponent(jLabel4))
+                    .addComponent(bt3)
+                    .addComponent(lb3))
                 .addGap(31, 31, 31)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton4)
-                    .addComponent(jLabel5))
+                    .addComponent(bt4)
+                    .addComponent(lb4))
                 .addGap(25, 25, 25))
         );
 
@@ -166,6 +189,36 @@ public class QUESTAO6 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bt3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt3ActionPerformed
+        new QUESTAO7().setVisible(true);
+        pontuacao +=10;
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bt3ActionPerformed
+
+    private void bt1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt1ActionPerformed
+        vidas -=1;
+        if (vidas > 0){
+        new QUESTAO7().setVisible(true);}
+        else new Fim().setVisible(true);
+// TODO add your handling code here:
+    }//GEN-LAST:event_bt1ActionPerformed
+
+    private void bt2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt2ActionPerformed
+        vidas -=1;
+        if (vidas > 0){
+        new QUESTAO7().setVisible(true);}
+        else new Fim().setVisible(true);
+// TODO add your handling code here:
+    }//GEN-LAST:event_bt2ActionPerformed
+
+    private void bt4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt4ActionPerformed
+        vidas -=1;
+        if (vidas > 0){
+        new QUESTAO7().setVisible(true);}
+        else new Fim().setVisible(true);
+// TODO add your handling code here:
+    }//GEN-LAST:event_bt4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -203,17 +256,17 @@ public class QUESTAO6 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton bt1;
+    private javax.swing.JButton bt2;
+    private javax.swing.JButton bt3;
+    private javax.swing.JButton bt4;
+    private javax.swing.JButton btVoltar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lb1;
+    private javax.swing.JLabel lb2;
+    private javax.swing.JLabel lb3;
+    private javax.swing.JLabel lb4;
+    private javax.swing.JLabel lbQuestao;
     // End of variables declaration//GEN-END:variables
 }
